03118047c179b44f980fe10d695271ee1780bc0d
Your Name <you@example.com>
2019-04-25 00:12:06 +1000

    pairing

---
+++ Shield For Dementia/ViewController/PairedViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PairedViewController.swift
+//  Shield For Dementia Patient
+//
+//  Created by Xiaocheng Peng on 24/4/19.
+//  Copyright © 2019 彭孝诚. All rights reserved.
+//
+
+import UIKit
+
+class PairedViewController: UIViewController {
+
+    var username:String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
